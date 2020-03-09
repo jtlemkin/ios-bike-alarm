ea0efb07c9c2e72b195b368244becc62b905871d
jtlemkin <jtlemkin@gmail.com>
2020-03-09 13:58:03 -0700

    Added bluetooth controller as observed property of main UI

--- IoBike/ContentView.swift
+++ IoBike/ContentView.swift
@@ -9,13 +9,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var bluetoothController: BluetoothController
+    
     var body: some View {
-        Text("Hello, World!")
+        Text(bluetoothController.isConnected ? "Connected" : "Disconnected")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(bluetoothController: BluetoothController())
     }
 }
