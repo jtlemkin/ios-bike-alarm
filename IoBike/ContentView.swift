0014751a2b9342e09535442930c1515399129976
jtlemkin <jtlemkin@gmail.com>
2020-03-09 12:43:59 -0700

    Initial Commit

---
+++ IoBike/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IoBike
+//
+//  Created by James Lemkin on 3/9/20.
+//  Copyright © 2020 James Lemkin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
