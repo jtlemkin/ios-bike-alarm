c4248433fc31a55180bb2a89c7c7e336bd399062
jtlemkin <jtlemkin@gmail.com>
2020-05-03 19:46:58 -0700

    Added SettingsView class

---
+++ IoBike/Views/SettingsView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsView.swift
+//  IoBike
+//
+//  Created by James Lemkin on 5/3/20.
+//  Copyright © 2020 James Lemkin. All rights reserved.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
