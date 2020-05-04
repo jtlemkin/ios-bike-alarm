84947388541237cb4755759e5ba7c5ef1696ee58
jtlemkin <jtlemkin@gmail.com>
2020-05-03 20:26:18 -0700

    Added title to settings

--- IoBike/Views/SettingsView.swift
+++ IoBike/Views/SettingsView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Settings")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.horizontal)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .topLeading)
+            
+            Spacer()
+        }
     }
 }
 
