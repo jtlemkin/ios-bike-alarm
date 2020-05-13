da9e3cb443ba996dbcaf7661bb3e46e032dbe20c
rsbhatia <rsbhatia@ucdavis.edu>
2020-05-13 13:23:52 -0700

    added reporting to firebase

--- IoBike/SupportingViews/SettingsViews/SettingsView.swift
+++ IoBike/SupportingViews/SettingsViews/SettingsView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import FirebaseDatabase
 
 struct SettingsView: View {
     @State private var isScanningQR = false
@@ -28,16 +29,29 @@ struct SettingsView: View {
             List {
                 SettingsRow(imageName: "arrow.up.arrow.down.circle.fill", text: "Set password", action: device.updatePassword)
                 SettingsRow(imageName: "plus.circle.fill", text: "Register New Device", action: { self.isScanningQR.toggle() })
-                SettingsRow(imageName: "exclamationmark.octagon.fill", text: "Report Stolen Bike", action: {})
+                SettingsRow(imageName: "exclamationmark.octagon.fill", text: "Report Stolen Bike", action: {
+                    var ref: DatabaseReference!
+                    ref = Database.database().reference()
+                    
+                    ref.child("UID").observeSingleEvent(of: .value, with: { (snapshot) in
+                        // Get user value
+                        let value = snapshot.value as? NSDictionary
+                        print(value)
+                        // ...
+                    }) { (error) in
+                        print(error.localizedDescription)
+                    }
+                })
+            }.sheet(isPresented: $isScanningQR) {
+                BikeRegistrationView()
             }
-        }.sheet(isPresented: $isScanningQR) {
-            BikeRegistrationView()
         }
     }
-}
-
-struct SettingsView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingsView(changeView: {})
+    
+    struct SettingsView_Previews: PreviewProvider {
+        static var previews: some View {
+            SettingsView(changeView: {})
+        }
     }
+    
 }
